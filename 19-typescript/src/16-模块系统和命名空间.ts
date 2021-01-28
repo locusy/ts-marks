0fc1078f163dbbc4435ed22fb3c20625931ec801
tianzhi <tianzhi@soyoung.com>
2021-01-28 18:58:16 +0800

    module

--- 19-typescript/src/16-模块系统和命名空间.ts
+++ 19-typescript/src/16-模块系统和命名空间.ts
@@ -1,13 +1,37 @@
 /**
  * 1、模块化
+ * 2、UMD规范代码
  */
 
-  /*****************************  1、模块化 ****************************/
-  // CMD
-  // commonjs规范
-  // nodejs
-  // module.exports
+/*****************************  1、模块化 ****************************/
+// CMD
+// commonjs规范
+// nodejs
+// module.exports
 
-  // AMD
-  // 浏览器
-  // require.js
+// AMD
+// 浏览器
+// require.js
+
+
+/*****************************  2、UMD规范代码 ****************************/
+// 由于ComminJS/AMD/CMD的差异兼容。随着Node.js的流行，前后端都可以用js进行开发，或多或少会出现前后端
+// 使用相同代码的情况，特别是不依赖宿主环境（浏览器，服务器）的偏底层代码。我们能实现一套多端适用（同构），
+// 在不同的模块化标准下使用。UMD就是一种解决方式。
+// (function(root, factory) {
+//   if(typeof module === 'object' && typeof module.exports === 'object') {
+//     module.exports = factory()
+//   } else if(typeof define === 'function' && define.amd) {
+//     define( factory )
+//   } else {
+//     root.methodName = factory
+//   }
+// })(this, function() {
+//   let a = 1;
+//   let b = 2;
+//   return {
+//     x: a,
+//     y: b
+//   }
+// })
+
